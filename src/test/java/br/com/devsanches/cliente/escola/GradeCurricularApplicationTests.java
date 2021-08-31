a0931665e7e12e865fdeb3fbf70ef55ce530048e
devsanches <chan.sanches@gmail.com>
2021-08-30 22:47:10 -0300

    Criação do Projeto

---
+++ src/test/java/br/com/devsanches/cliente/escola/GradeCurricularApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.devsanches.cliente.escola;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GradeCurricularApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
